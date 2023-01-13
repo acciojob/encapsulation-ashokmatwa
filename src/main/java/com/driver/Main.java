dbab0177158b20a9c2ae3856e4434c288dd08937
Ashok Matwa <ashokmatwa003@gmail.com>
2023-01-13 12:56:35 +0530

    check again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,9 +12,3 @@ public class Main {
         System.out.println(obj.getName()); //getter function called
     }
 }
-/*
-* Learnings:
-Encapsulation is used for hiding data,
-you can create a readOnly member, by just implementing getter function for that
-Or you can also create a writeOnly member, by just implementing setter function for that
-* */
