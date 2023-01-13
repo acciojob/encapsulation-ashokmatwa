a701916979aea8740127e36a66f0baf2f4166063
Ashok Matwa <ashokmatwa003@gmail.com>
2023-01-13 12:52:57 +0530

    check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // name has private access in com.driver.RWOnly
+//        obj.name = "Ashok";
+//        System.out.println(obj.name);
+
+        obj.setName("Ashok"); //setter function called
+        System.out.println(obj.getName()); //getter function called
+    }
 }
+/*
+* Learnings:
+Encapsulation is used for hiding data,
+you can create a readOnly member, by just implementing getter function for that
+Or you can also create a writeOnly member, by just implementing setter function for that
+* */
